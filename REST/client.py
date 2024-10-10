8826b99c4f59dbd9f689f445da53d60ce0e9374c
Monir Rafik <159887885+mraf01@users.noreply.github.com>
2024-10-10 18:54:49 +0200

    REST

--- REST/client.py
+++ REST/client.py
@@ -1,8 +1,10 @@
-import requests, json, sys
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import sys
 
-
-base_url = "https://127.0.0.1:8080"
-
+# base_url = "https://127.0.0.1:8080"
+base_url = "https://172.24.79.117:8080"
 
 def GetDatiCittadino():
     nome = input("Inserisci il nome: ")
@@ -17,22 +19,77 @@ def GetDatiCittadino():
     }
     return datiCittadino
 
-
 def GetCodicefiscale():
     cod = input('Inserisci codice fiscale: ')
     return {"codice fiscale": cod}
 
+def GetAuthCredentials():
+    username = input("Inserisci il nome utente: ")
+    password = input("Inserisci la password: ")
+    return username, password
 
+def Operazioni(username, password, privilegi):
+    while True:
+        print("\nOperazioni disponibili:")
+        if privilegi == 'r':
+            print("2. Richiedi cittadino")
+            print("5. Esci")
+        elif privilegi == 'w':
+            print("1. Inserisci cittadino")
+            print("2. Richiedi cittadino")
+            print("3. Modifica cittadino")
+            print("4. Elimina cittadino")
+            print("5. Esci")
+
+        try:
+            sOper = int(input("Cosa vuoi fare? "))
+        except ValueError:
+            print("Inserisci un numero valido!")
+            continue
+
+        if privilegi == 'r' and sOper != 2:
+            print("Non hai il permesso di eseguire questa operazione.")
+            continue
+
+        if sOper == 1 and privilegi == 'w':
+            print("Aggiunta cittadino")
+            api_url = base_url + "/add_cittadino"
+            jsonDataRequest = GetDatiCittadino()
+            response = requests.post(api_url, json=jsonDataRequest, auth=HTTPBasicAuth(username, password), verify=False)
+            print(response.json())
+
+        elif sOper == 2:
+            print("Richiesta dati cittadino")
+            api_url = base_url + "/read_cittadino"
+            jsonDataRequest = GetCodicefiscale()
+            response = requests.post(api_url, json=jsonDataRequest, auth=HTTPBasicAuth(username, password), verify=False)
+            print(response.json())
+
+        elif sOper == 3 and privilegi == 'w':
+            print("Modifica cittadino")
+            api_url = base_url + "/update_cittadino"
+            jsonDataRequest = GetDatiCittadino()
+            response = requests.post(api_url, json=jsonDataRequest, auth=HTTPBasicAuth(username, password), verify=False)
+            print(response.json())
+
+        elif sOper == 4 and privilegi == 'w':
+            print("Eliminazione cittadino")
+            api_url = base_url + "/elimina_cittadino"
+            jsonDataRequest = GetCodicefiscale()
+            response = requests.post(api_url, json=jsonDataRequest, auth=HTTPBasicAuth(username, password), verify=False)
+            print(response.json())
+
+        elif sOper == 5:
+            print("Buona giornata!")
+            sys.exit()
+
+        else:
+            print("Operazione non disponibile, riprova.")
 
 while True:
     print("\nOperazioni disponibili:")
-    print("1. Inserisci cittadino")
-    print("2. Richiedi cittadino")
-    print("3. Modifica cittadino")
-    print("4. Elimina cittadino")
-    print("5. Controllo cittadino")
-    print("6. Esci")
-
+    print('1. login utente')
+    print('2. esci')
 
     try:
         sOper = int(input("Cosa vuoi fare? "))
@@ -40,46 +97,39 @@ while True:
         print("Inserisci un numero valido!")
         continue
 
-
     if sOper == 1:
-        print("Aggiunta cittadino")
-        api_url = base_url + "/add_cittadino"
-        jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url, json=jsonDataRequest, verify=False)
+        api_url = base_url + '/login_utente'
+        username, password = GetAuthCredentials()
+        
+        
+        jsonDataRequest = {"user": username, "password": password}
+        
+        try:
+            response = requests.post(api_url, json=jsonDataRequest, verify=False)
+            response.raise_for_status()  # Verifica se la richiesta ha avuto successo (es. status code 200)
+        except requests.exceptions.RequestException as e:
+            print(f"Errore nella richiesta: {e}")
+            continue
 
+        try:
+            jsonResp = response.json()
+        except ValueError:
+            print("Errore di decodifica JSON. Controlla la risposta del server.")
+            print("Response text:", response.text)
+            continue
+
+        esito = jsonResp.get("login")
+        privilegi = jsonResp.get("privilegi")
+
+        if esito:
+            print("Login effettuato con successo!")
+            Operazioni(username, password, privilegi)  
+            break
+        else:
+            print("Errore di login: ", jsonResp.get("Msg"))
+            print('Riprova')
     elif sOper == 2:
-        print("Richiesta dati cittadino")
-        api_url = base_url + "/read_cittadino"
-        jsonDataRequest = GetCodicefiscale()
-        response = requests.post(api_url, json= jsonDataRequest, verify=False)
-        print(response.json())
-
-    elif sOper == 3:
-        print("Modifica cittadino")
-        api_url = base_url + "/update_cittadino"
-        jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url, json=jsonDataRequest, verify=False)
-        print(response.json())
-
-
-    elif sOper == 4:
-        print("Eliminazione cittadino")
-        api_url = base_url + "/elimina_cittadino"
-        jsonDataRequest = GetCodicefiscale()
-        response = requests.post(api_url, json=jsonDataRequest, verify=False)
-        print(response.json())
-    
-    elif sOper ==5:
-        print('controllo cittadino')
-        api_url = base_url + '/login_cittadino'
-        jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url, json=jsonDataRequest, verify=False)
-        print(response.json())
-
-
-    elif sOper == 6:
         print("Buona giornata!")
         sys.exit()
-
     else:
         print("Operazione non disponibile, riprova.")
